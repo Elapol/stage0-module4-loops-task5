f36c21f2a65660cd9b1ba0f5c1c856dae2294b74
elyad <ella.dikovich@gmail.com>
2022-12-04 14:23:59 +0100

    elka1

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -6,23 +6,23 @@ public class Hourglass {
     }
 
     public static void printHourglassOfGivenSize(int height) {
-        for (int x = 1; x < height; x++) {
-            for (int y = 1; y < x; y++) {
+        for (int x = 0; x < height/2; x++) {
+            for (int y = 0; y < x; y++) {
                 System.out.print(" ");
             }
-            for (int z = height; z >= x; z--) {
-                System.out.print("8 ");
+            for (int z =0; z<(height-x*2)-1; z++) {
+                System.out.print("8");
             }
 
             System.out.println();
         }
-            for (int x = 0; x < height; x++) {
-                for (int y = height - 1; y > x; y--) {
+            for (int x = 1; x <= height/2; x++) { //1
+                for (int y = height/2; y > x; y--) { //2
 
                     System.out.print(" ");
                 }
-                for (int z = 0; z <= x; z++) {
-                    System.out.print("8 ");
+                for (int z = 0; z <x*2-1; z++) {
+                    System.out.print("8");
                 }
                 System.out.println();
             }
