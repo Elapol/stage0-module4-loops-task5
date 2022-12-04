29398d72198a7f39d75623e030db9f87ca7b09dd
elyad <ella.dikovich@gmail.com>
2022-12-04 21:49:55 +0100

    eliczka23

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -2,36 +2,50 @@ package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
     public static void main(String[] args) {
-        printHourglassOfGivenSize(5);
+        printHourglassOfGivenSize(3);
     }
 
     public static void printHourglassOfGivenSize(int height) {
 
-            for (int x = 0; x < height / 2; x++) {
-                for (int y = height - x; y < height; y++) {
-                    System.out.print(" ");
-                }
-                for (int z = 0; z < (height - 2 * x); z++) {
-                    System.out.print("8");
-                }
-
-                System.out.println();
+        for (int x = 0; x < height / 2; x++) {
+            for (int y = height - x; y < height; y++) {
+                System.out.print(" ");
+            }
+            for (int z = 0; z < (height - 2 * x); z++) {
+                System.out.print("8");
             }
 
+            System.out.println();
+        }
 
 
-            for (int x = 2; x >=0; x--) { //1
-                for (int y = height-x; y<height; y++) { //2
+        if (height % 2 == 0) {
+            for (int x = height / 2 - 1; x >= 0; x--) {
+
+                for (int y = height - x; y < height; y++) { //2
 
                     System.out.print(" ");
                 }
-                for (int z = 0; z <height-(x*2); z++) {
+                for (int z = 0; z < height - (x * 2); z++) {
+                    System.out.print("8");
+                }
+                System.out.println();
+            }
+        } else {
+            for (int x = height / 2; x >= 0; x--) {
+
+                for (int y = height - x; y < height; y++) { //2
+
+                    System.out.print(" ");
+                }
+                for (int z = 0; z < height - (x * 2); z++) {
                     System.out.print("8");
                 }
                 System.out.println();
             }
         }
     }
+}
 
 
 
